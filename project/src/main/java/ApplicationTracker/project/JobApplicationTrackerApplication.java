4efe8eef62a04028596de08d0cc96df41cecdcab
Aayan Sayed <aas1128@rit.edu>
2025-02-08 14:33:48 -0500

    start of project, configured postgresSQL

---
+++ project/src/main/java/ApplicationTracker/project/JobApplicationTrackerApplication.java
@@ -0,0 +1,13 @@
+package ApplicationTracker.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationTrackerApplication.class, args);
+	}
+
+}
